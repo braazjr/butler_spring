39c97fd2db42b052fda317871e2bf630c2a88207
ebj <ebj@CAC-D-118.DETIN.CAC>
2017-12-13 09:54:09 -0200

    Atualização do projeto

--- src/main/java/br/com/onsmarttech/butler/resource/EmpresaController.java
+++ src/main/java/br/com/onsmarttech/butler/resource/EmpresaController.java
@@ -9,15 +9,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import br.com.onsmarttech.butler.models.Empresa;
 import br.com.onsmarttech.butler.repositories.EmpresaRepository;
+import br.com.onsmarttech.butler.service.EmpresaService;
 
 @RestController
 @RequestMapping("/empresas")
@@ -26,28 +29,24 @@ public class EmpresaController {
 	@Autowired
 	private EmpresaRepository repository;
 
-	@GetMapping
-	public ModelAndView listaEmpresas() {
-		ModelAndView view = new ModelAndView("empresa/listaEmpresas");
-		view.addObject("empresas", repository.findAll());
-		return view;
-	}
+	@Autowired
+	private EmpresaService service;
 
-	@GetMapping("/cadastro")
-	public ModelAndView cadastroEmpresa() {
-		ModelAndView view = new ModelAndView("empresa/cadastroEmpresa");
-		view.addObject("empresa", new Empresa());
-		return view;
+	@GetMapping
+	public ResponseEntity<List<Empresa>> listaEmpresas() {
+		List<Empresa> empresas = repository.findAll();
+
+		return empresas != null ? ResponseEntity.ok(empresas) : ResponseEntity.notFound().build();
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<?> buscarEmpresaPorId(@PathVariable Long id) {
 		Empresa empresa = repository.findOne(id);
-		
+
 		return empresa != null ? ResponseEntity.ok(empresa) : ResponseEntity.notFound().build();
 	}
 
-	@PostMapping(value = "/cadastro")
+	@PostMapping
 	public ResponseEntity<?> salvarEmpresa(@Valid Empresa empresa, BindingResult bindingResult) {
 		Empresa empresaSalva = repository.save(empresa);
 
@@ -56,5 +55,18 @@ public class EmpresaController {
 		}
 
 		return new ResponseEntity<Empresa>(empresaSalva, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long id) {
+		repository.delete(id);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<?> salvarEmpresa(@PathVariable Long id, @Valid Empresa empresa, BindingResult bindingResult) {
+		Empresa empresaSalva = service.atualizar(id, empresa);
+
+		return ResponseEntity.ok(empresaSalva);
 	}
 }
---
+++ src/main/java/br/com/onsmarttech/butler/service/EmpresaService.java
@@ -0,0 +1,28 @@
+package br.com.onsmarttech.butler.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import br.com.onsmarttech.butler.models.Empresa;
+import br.com.onsmarttech.butler.repositories.EmpresaRepository;
+
+@Service
+public class EmpresaService {
+
+	@Autowired
+	private EmpresaRepository repository;
+
+	public Empresa atualizar(Long codigo, Empresa empresa) {
+		Empresa empresaSalva = repository.findOne(codigo);
+
+		if (empresaSalva == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+
+		BeanUtils.copyProperties(empresa, empresaSalva, "id");
+
+		return repository.save(empresaSalva);
+	}
+}
